ce86d5527fba1c82c27b8579c8bebb66941281ac
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-02-17 20:38:08 +0500

    2D array problem

--- 8. Pointers/7. Passing A Two Dimensional Array/Untitled1.cpp
+++ 8. Pointers/7. Passing A Two Dimensional Array/Untitled1.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+
+int prints(){
+
+}
+
 int main(){
-	cout<<"Hello, World";
+	/*/What are 2 Dinmensional Array?
+	the below array meanns that	we are creating two arrays of 
+	3 elements each.
+	/*/
+	int places[2][3]={{1,2,3},{4,5,6}};
+
+	//instead of integar now the 2 in places represents 2 array.
+	//so when we use (&B+1) Instead of going to next integar we are going towards 
+	//the next array which also have three integers
+	
+
 }
