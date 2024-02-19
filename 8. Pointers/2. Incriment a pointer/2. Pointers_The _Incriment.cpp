442797a39331c6488178ffc1202b947a0e568f44
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-02-19 16:44:59 +0500

    Pointers incriments

--- 8. Pointers/2. Incriment a pointer/2. Pointers_The _Incriment.cpp
+++ 8. Pointers/2. Incriment a pointer/2. Pointers_The _Incriment.cpp
@@ -1,5 +1,28 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int input_Array(int array[],int size){
+    for (int i = 0; i <size; i++)
+    {
+        cout<<"Your "<<i+1<<" Number is: "<<endl;
+        cin>>array[i];
+    }
     
+}
+
+int Output_Array(int array[],int size){
+    cout<<"Your Array Numbers Are: ";
+    for (int i = 0; i <size; i++){
+    cout<<array[i];
+    cout<<" ,";
+    }
+}
+
+int main(){
+    int array[2];
+    int size= sizeof(array)/sizeof(array[0]);
+    input_Array(array,size);
+    Output_Array(array,size);
+
+
 }
