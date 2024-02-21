b13bbdec064c32aa1374763ff84ce9bc981cbdf5
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-02-21 05:36:30 +0500

    Function That Tells Prime Number

---
+++ 6. Functions/5. prime composite function/U.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+//function to check wether a number is even or odd!
+void isprime(int num){
+    for(int i=2;i<=num/2;i++) {
+	if(num%i==0){
+		cout<<num<<" is composite.";
+	}else{
+		cout<<num<<" is prime";
+	}
+    }
+    
+	}
+
+
+//Start Of Program:
+int main(){
+	int num;
+	cin>>num;
+	isprime(num);
+	return 0;
+}
