a4f8c899ff3b1bdb6b427818b9056295dc664bfb
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-02-26 15:51:52 +0500

    Changing Variable Name

--- 7. Arrays/7. Multi dimensional array/Untitled2.cpp
+++ 7. Arrays/7. Multi dimensional array/Untitled2.cpp
@@ -9,7 +9,7 @@ int main(){
 	int sub_Marks[30][7];
 	
 	//there are 5 classes which each have 30 students and there marks in 7 subjects:
-	int class[5][30][7];
+	int classpro[5][30][7];
 	//there are three more schools like that ,which have 5 class with 30 students
 	int schools[3][5][30][7];
 	
