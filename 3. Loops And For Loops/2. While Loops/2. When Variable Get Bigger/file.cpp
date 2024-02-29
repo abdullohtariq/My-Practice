aab74d8afd8024ac1fe358145b1ad5ab9938b013
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-01 03:48:08 +0500

    While Loop With a fasle condition

---
+++ 3. Loops And For Loops/2. While Loops/2. When Variable Get Bigger/file.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter Your Number: "<<endl;
+    int Number=0 ;
+    while(Number!=10){
+        cout<<"Hello, World"<<endl;
+        Number++;
+    }
+    }
