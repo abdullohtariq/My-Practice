ccced507c961955701496119b633db11d55caec7
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-01 03:37:47 +0500

    Do While Loop

---
+++ 3. Loops And For Loops/3. Do While Loops/1. Do While Loops/2. A loop which don't run/file.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter Your Number: "<<endl;
+    int Number; cin>>Number;
+    do{
+        cout<<Number<<endl;
+    }while(0>1);
+    //it will still print your number one time even if the condition is false.
+    }
