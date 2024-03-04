faa0fc9612b58cc45ce36ba5d8c4a75bd5b26902
abdullohtariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-04 10:12:28 +0500

    Pointer To Pointers Variables

---
+++ 8. Pointers/3. Pointer to Pointer/2. Working With Function/Untitled1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+void squarePointer1(int *p){
+	*p =*p * *p;
+}
+void unsquarePointer2(int **q){
+	**q=*(*q) * *(*q);
+}
+void squarePointer3(int ***r){
+	***r=***r * ***r;
+}
+
+
+
+int main(){
+	cout<<"Enter a Number You Want a Square off: ";
+	int kali;
+	cin>>kali;
+	int *p;
+	p=&kali;
+	int**q;
+	q=&p;
+	int ***r;
+	r=&q;
+	//address of the variable:  
+	cout<<"Address of the Variable: "<<endl<<endl;
+	cout<<"The Address of kali using kali variable: "<<&kali<<endl;
+	cout<<"The Address of Kali Using Single Pointer Variable: "<<p<<endl;
+	cout<<"The Address of Kali Using pointer to Pointer Variable: "<<*q<<endl;
+	cout<<"The Address of Kali Using pointer to pointer to Pointer Variable: "<<**r<<endl;
+	
+	//address of single pointer variable  
+	cout<<"Address of the Variable: "<<endl<<endl;
+	cout<<"The Address of p using kali variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of p Using Single Pointer Variable: "<<&p<<endl;
+	cout<<"The Address of p Using pointer to Pointer Variable: "<<q<<endl;
+	cout<<"The Address of p Using pointer to pointer to Pointer Variable: "<<*r<<endl;
+	//address to pointer to pointer:
+	cout<<"Address of the Variable: "<<endl<<endl;
+	cout<<"The Address of q using kali variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of q Using Single Pointer Variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of q Using pointer to Pointer Variable: "<<&q<<endl;
+	cout<<"The Address of q Using pointer to pointer to Pointer Variable: "<<r<<endl;
+	//adress to pointer to pointer to pointer variable: 
+	cout<<"Address of the Variable: "<<endl<<endl;
+	cout<<"The Address of q using kali variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of q Using Single Pointer Variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of q Using pointer to Pointer Variable: "<<"X (can't be done)"<<endl;
+	cout<<"The Address of q Using pointer to pointer to Pointer Variable: "<<&r<<endl;
+	//value of variable: 
+	cout<<"Value of Variable Using variable: "<<kali<<endl;
+	cout<<"Value of Variable Using pointer variable: "<<*p<<endl;
+	cout<<"Value of Variable Using pointer to pointer variable: "<<**q<<endl;
+	cout<<"Value of Variable Using pointer to pointer to pointer variable: "<<***r<<endl;
+	
+	//we can use either pointer to get square or un square of the number:
+	
+	squarePointer1(p);
+	cout<<"Using *p Variable And Square1 Function: "<<kali<<endl;
+	unsquarePointer2(q);
+	cout<<"Using **q Variable And square2 Function: "<<kali<<endl;
+	squarePointer3(r);
+	cout<<"Using **q Variable And square3 Function: "<<kali<<endl;
+	
+}
