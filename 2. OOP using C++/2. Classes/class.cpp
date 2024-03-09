199e1fd5918828d505d2be5976e0088797a04cb5
Abdullah Tariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-09 21:03:43 +0500

    Class In OOP

---
+++ 2. OOP using C++/2. Classes/class.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//Class (Atributes/Behaviours):
+
+/*
+A Class is the buliding block of the OOP. 
+A Class is User Define Data Type.
+
+A Class is little more complex than normal predefined Variables.
+You Can Show Atributes and Behaviours in classes to show Atributets you need mutipule pre-defined variables which make a group and than
+make the atributes.
+behaviour of the class is defined by the functions inside the class.
+*/
+
+class Empolyee{
+    string Name;
+    string Company;
+    int Age;
+};
+
+int main(){
+    //Now You can use the above class of Employee As Data Type.
+    Empolyee employee1;
+
+}
