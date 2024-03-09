994648279239dd4777e53c65058572dda1f7cd9f
Abdullah Tariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-09 20:21:52 +0500

    Intro To OOP

---
+++ 10. OOP using C++/1. Introduction/Intro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//OOP is programing Paradigm
+
+/* 
+Which Means, it is set of rules, concept and ideas or a standard that we follow
+in programming to slove a specific problem 
+*/
+
+//There are more than one programming paradigm. (OOP is just one of em.)
+
+
+
+int main(){
+    //for example there is a car that we need a program of the car has many atributes and behaviours 
+    //to convert all the arrtributes and the behaviours we use basic buliding block of OOP "Which is CLass"
+    //What is a Class?{Check in next folder}
+    cout<<"This program include intro to OOP.";
+
+}
