30399a872eceb6f38230362e6460e0641749a2cb
Abdullah Tariq <159859272+abdullohtariq@users.noreply.github.com>
2024-03-13 20:09:45 +0500

    code

---
+++ 1. Programming Fundamentals/4. Nested Loops/iv. One more pattern/pateern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int rows = 5;  // Number of rows (and columns) in the square
+
+  for (int i = 1; i <= rows; ++i) {
+    for (int j = 1; j <= rows; ++j) {
+      cout << "*";
+    }
+    cout << endl;  // Move to a new line after each row
+  }
+
+  return 0;
+}
