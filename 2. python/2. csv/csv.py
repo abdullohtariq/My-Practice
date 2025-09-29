199f706d65edad0263a17f963f6c9ab98a2a0845
abdullohtariq <abdollahtariq@gmail.com>
2025-09-29 15:38:05 +0500

    learning and using lambda funtion to making the code mre usable

--- 2. python/2. csv/csv.py
+++ 2. python/2. csv/csv.py
@@ -3,11 +3,11 @@ def main():
     with open('nameHouse.csv', 'r') as file:
         for line in file:
             line = line.strip().split(',')
-            student = {}
-            student['name'] = line[0]
-            student['house'] = line[1]
+            student = {"name": line[0], "house": line[1]}
             students.append(student)
-    for student in students:
+
+
+    for student in sorted(students, key = lambda student : student["name"], reverse= True):
         print(f"{student['name']} is in {student['house']}")
 
 
