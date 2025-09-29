5eeb2aeebde6a59af68bec6166a52d200a170a61
abdullohtariq <abdollahtariq@gmail.com>
2025-09-29 15:11:06 +0500

    Refactor CSV reading to store student data in a list and print formatted output

--- 2. python/2. csv/csv.py
+++ 2. python/2. csv/csv.py
@@ -1,8 +1,14 @@
 def main():
+    students = []
     with open('nameHouse.csv', 'r') as file:
         for line in file:
-            line = line.rsplit().split(',')
-            print("Hello, " + line[0]+ " WHo Lives in " + line[1] + "!")
+            line = line.strip().split(',')
+            student = {}
+            student['name'] = line[0]
+            student['house'] = line[1]
+            students.append(student)
+    for student in students:
+        print(f"{student['name']} is in {student['house']}")
 
 
 if __name__ == "__main__":
