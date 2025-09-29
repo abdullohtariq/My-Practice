4bee91cd4cfe43dfc6df46d352f6753f68ea0013
abdullohtariq <abdollahtariq@gmail.com>
2025-09-29 16:18:35 +0500

    Refactor student data handling by implementing CSV reading with DictReader and sorting output by name

--- 2. python/2. csv/student.py
+++ 2. python/2. csv/student.py
@@ -1,9 +1,11 @@
+import csv
+
 def main():
     students = []
-    with open('nameHouse.csv', 'r') as file:
-        for line in file:
-            line = line.strip().split(',')
-            student = {"name": line[0], "house": line[1]}
+    with open('nameHouse.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            student = {"name": row["Name"], "house": row["House"]}
             students.append(student)
 
 
