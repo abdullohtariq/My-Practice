83f25e093dc6a81bdc8febd000cd615adc722ded
abdullohtariq <abdollahtariq@gmail.com>
2025-10-01 02:02:20 +0500

    Fix email validation regex to allow subdomains and dots in username

--- 2. python/5. regular expressions/email.py
+++ 2. python/5. regular expressions/email.py
@@ -1,7 +1,7 @@
 import re
 def main():
     email = input("Enter your email: ")
-    if re.search(r"^[a-zA-Z0-1_]+@[a-zA-Z0-1_]+\.edu$", email, re.IGNORECASE):
+    if re.search(r"^([\w\.]+)@(\w+)(\.\w+)?\.edu$", email, re.IGNORECASE):
         print("Valid email")
     else:
         print("Invalid email")
