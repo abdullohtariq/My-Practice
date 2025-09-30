525fca2ab3d8fcf12d5ad118574670a28622bacc
abdullohtariq <abdollahtariq@gmail.com>
2025-09-30 13:17:38 +0500

    Fix CSV formatting and improve data entry script for consistent line endings

--- 2. python/write/write.py
+++ 2. python/write/write.py
@@ -3,8 +3,8 @@ def main():
     name = input("Enter your name: ")
     age = input("Enter your age: ")
     home = input("Enter your home: ")
-    with open("data.csv", "a") as file:
-        writer = csv.DictWriterwriter(file, fieldnames=["name", "age", "address"])
+    with open("data.csv", "a", newline= "") as file:
+        writer = csv.DictWriter(file, fieldnames=["name", "age", "address"], lineterminator="\n")
         writer.writerow({"name": name, "age": age, "address": home})
         
 if __name__ == "__main__":
