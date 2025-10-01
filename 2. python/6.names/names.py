7d20fd38bc69c72ebf05feb255b693e3c2113acc
abdullohtariq <abdollahtariq@gmail.com>
2025-10-01 15:16:11 +0500

    Update greeting.txt with additional names and refine regex for name validation

--- 2. python/6.names/names.py
+++ 2. python/6.names/names.py
@@ -3,10 +3,10 @@ import sys
 def main():
     name = input("Enter your name: ")
     last, first = "", ""
-    if re.match(r"^\w+, ?\w+$", name):
+    if re.match(r"^[a-zA-Z_]+, ?[a-zA-Z_]+$", name):
         last, first = re.split(r", ?", name)
     
-    elif re.match(r"^\w+ \w+$", name):
+    elif re.fullmatch(r"[a-zA-Z_]+ [a-zA-Z_]+", name):
         first, last = name.split(" ")
         
     else:
