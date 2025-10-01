952a012a2595a3d5c6376e344c0f013657146b6e
abdullohtariq <abdollahtariq@gmail.com>
2025-10-01 15:40:48 +0500

    Refactor name input handling to improve format validation and streamline greeting file updates

--- 2. python/6.names/names.py
+++ 2. python/6.names/names.py
@@ -2,21 +2,16 @@ import re
 import sys
 def main():
     name = input("Enter your name: ")
-    last, first = "", ""
-    if re.match(r"^[a-zA-Z_]+, ?[a-zA-Z_]+$", name):
-        last, first = re.split(r", ?", name)
-    
-    elif re.fullmatch(r"[a-zA-Z_]+ [a-zA-Z_]+", name):
-        first, last = name.split(" ")
-        
-    else:
-        sys.exit("Invalid name format")
+    matches = re.search(r"^([\w .'-]+), ?([\w .'-]+)$", name)
+    if matches:
+        last = matches.group(1)
+        first = matches.group(2)
+        name = f"{first} {last}"
         
     with open("greeting.txt", "a") as file:
-        file.write(f"{first} {last}\n")
+        file.write(f"{name}\n")
 
         
-
 
 if __name__ == "__main__":
     main()
