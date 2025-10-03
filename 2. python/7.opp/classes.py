51ab95e35dccec15ee269570a19fb3201ec0a7b8
abdullohtariq <abdollahtariq@gmail.com>
2025-10-03 17:41:56 +0500

    making a student class with two required attributes or intance variables

--- 2. python/7.opp/classes.py
+++ 2. python/7.opp/classes.py
@@ -1,5 +1,20 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 def main():
-    pass
+    student = get_student()
+    print(f"Name: {student.name}")
+    print(f"Age: {student.age}")
+
+
+def get_student():
+    #makintg object of class
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
+    student = Student(name, age)
+    return student
 
 if __name__ == "__main__":
     main()
